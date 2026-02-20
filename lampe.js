5c10be494eaf3966668f82a01384d8af764d13c9
Linna-dotcom <linnalissy11@gmail.com>
2026-02-20 19:41:28 +0100

    lampe

--- lampe.js
+++ lampe.js
@@ -1,19 +1,14 @@
-document.addEventListener('DOMContentLoaded', funktion (),{
-    var :handleClick = function (){
-        if (document.querySelector('#Lampe').style.backroundImage === 'url("IMG_1944.png")'){
-            document.querySelector('#Lampe').style.backroundImage === 'url("IMG_1943.png")';
-            document.querySelector('#Lampe').style.backroundColor = 'rgb(255, 234, 0)';
-        }else{
-            document.querySelector('#Lampe').style.backroundImage === 'url("IMG_1944.png")';
-            document.querySelector('#Lampe').style.backroundColor = 'grey';
+document.addEventListener('DOMContentLoaded', function () {
+    var handleClick = function () {
+        if (document.querySelector('#Lampe').style.backgroundImage === 'url("IMG_1943.png")') {
+            document.querySelector('#Lampe').style.backgroundImage = 'url("IMG_1944.png")';
+            document.querySelector('#Lampe').style.backgroundColor = 'rgb(255, 255, 255)';
+        } else {
+            document.querySelector('#Lampe').style.backgroundImage = 'url("IMG_1943.png")';
+            document.querySelector('#Lampe').style.backgroundColor = 'grey';
         }
-    },
-    var :button = document.querySelector('#Birne'),
-    button,addEventListener('click', handleClick){
-
-    }
+    };
     
-
-
-
+    var button = document.querySelector('#Birne');
+    button.addEventListener('click', handleClick);
 });
