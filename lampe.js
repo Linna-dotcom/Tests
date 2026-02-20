17653d16bc23eee1c0b6ccb10041061f0e0fc978
Linna-dotcom <linnalissy11@gmail.com>
2026-02-20 13:36:19 +0100

    lampe

---
+++ lampe.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', funktion (){
+    var handleClick = function (){
+        if (document.querySelector('#Lampe').style.backroundImage === 'url("IMG_1944.png")'){
+            document.querySelector('#Lampe').style.backroundImage === 'url("IMG_1943.png")';
+            document.querySelector('#Lampe')style.backroundColor = 'rgb(255, 234, 0)';
+        }else{
+            document.querySelector('#Lampe').style.backroundImage === 'url("IMG_1944.png")';
+            document.querySelector('#Lampe')style.backroundColor = 'grey';
+        }
+    }
+        var button = document.querySelector('#Birne');
+        button.addEventListener('click', handleClick);
+
+
+
+
+
+    }
+
+
+})
