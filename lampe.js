0169489024429b2dc94116b2b9a8d1eacb53b0ae
Linna-dotcom <linnalissy11@gmail.com>
2026-02-20 14:02:30 +0100

    lampe

--- lampe.js
+++ lampe.js
@@ -1,15 +1,18 @@
-document.addEventListener('DOMContentLoaded', funktion (){
+document.addEventListener('DOMContentLoaded', funktion (),{
     var :handleClick = function (){
         if (document.querySelector('#Lampe').style.backroundImage === 'url("IMG_1944.png")'){
             document.querySelector('#Lampe').style.backroundImage === 'url("IMG_1943.png")';
-            document.querySelector('#Lampe')style.backroundColor = 'rgb(255, 234, 0)';
+            document.querySelector('#Lampe').style.backroundColor = 'rgb(255, 234, 0)';
         }else{
             document.querySelector('#Lampe').style.backroundImage === 'url("IMG_1944.png")';
-            document.querySelector('#Lampe')style.backroundColor = 'grey';
+            document.querySelector('#Lampe').style.backroundColor = 'grey';
         }
     },
-    var :button = document.querySelector('#Birne');
-    button.addEventListener('click', handleClick);
+    var :button = document.querySelector('#Birne'),
+    button,addEventListener('click', handleClick){
+
+    }
+    
 
 
 
