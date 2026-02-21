94b9d5450fff6dbc4cbc4571c72b696e58708a73
Linna-dotcom <linnalissy11@gmail.com>
2026-02-21 18:07:18 +0100

    lampe

--- lampe.js
+++ lampe.js
@@ -1,14 +1,37 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var handleClick = function () {
-        if (document.querySelector('#Lampe').style.backgroundImage === 'url("IMG_1943.png")') {
-            document.querySelector('#Lampe').style.backgroundImage = 'url("IMG_1944.png")';
-            document.querySelector('#Lampe').style.backgroundColor = 'rgb(255, 234, 0)';
-        } else {
-            document.querySelector('#Lampe').style.backgroundImage = 'url("IMG_1943.png")';
-            document.querySelector('#Lampe').style.backgroundColor = 'grey';
-        }
-    };
-    
+document.addEventListener('DOMContentLoaded', function() {
+
+var handleClick = function () {
+    if (document.querySelector('#Lampe').style.backgroundImage === 'url("../Linna_Mackenbrock_Portfolio/img/licht-aus.png")') {
+        document.querySelector('#Lampe').style.backgroundImage = 'url("../Linna_Mackenbrock_Portfolio/img/licht-an.png")'; 
+        document.querySelector('#Lampe').style.backgroundColor = 'rgb(255, 234, 0)';
+
+    } else {
+        document.querySelector('#Lampe').style.backgroundImage = 'url("../Linna_Mackenbrock_Portfolio/img/licht-aus.png")';
+        document.querySelector('#Lampe').style.backgroundColor = 'grey';
+    }
+
+
+} 
     var button = document.querySelector('#Birne');
+
     button.addEventListener('click', handleClick);
+
+
+
+
+
+var handleClick = function () {
+    if (document.querySelector('#Birne').style.color === 'grey') {
+         document.querySelector('#Birne').style.color = 'rgb(255, 234, 0)';  
+    } else {
+        document.querySelector('#Birne').style.color = 'grey';
+    }
+
+    //change colour to grey
+
+};
+var button = document.querySelector('#Birne');
+
+button.addEventListener('click', handleClick);
+
 });
