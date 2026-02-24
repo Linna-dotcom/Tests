c6199f2603229d5e6371ae21fb37a4adee938904
Linna-dotcom <linnalissy11@gmail.com>
2026-02-24 09:17:27 +0100

    lampe

--- lampe.js
+++ lampe.js
@@ -1,17 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function(){
 
 var handleClick = function () {
-    if (document.querySelector('#Lampe').style.backgroundImage === 'url("IMG_1943")') {
-        document.querySelector('#Lampe').style.backgroundImage = 'url("IMG_1944")'; 
+    if (document.querySelector('#Lampe').style.backgroundImage === 'url("IMG_1944.png")') {
+        document.querySelector('#Lampe').style.backgroundImage = 'url("IMG_1943.png")';
         document.querySelector('#Lampe').style.backgroundColor = 'rgb(255, 234, 0)';
-
+    
     } else {
-        document.querySelector('#Lampe').style.backgroundImage = 'url("IMG_1943")';
+        document.querySelector('#Lampe').style.backgroundImage = 'url("IMG_1943.png")';
         document.querySelector('#Lampe').style.backgroundColor = 'grey';
     }
 
-
-} 
+}
     var button = document.querySelector('#Birne');
 
     button.addEventListener('click', handleClick);
@@ -20,14 +19,15 @@ var handleClick = function () {
 
 
 
+
 var handleClick = function () {
     if (document.querySelector('#Birne').style.color === 'grey') {
-         document.querySelector('#Birne').style.color = 'rgb(255, 234, 0)';  
+        document.querySelector('#Birne').style.color = 'rgb(255, 234, 0)';
     } else {
         document.querySelector('#Birne').style.color = 'grey';
     }
 
-    //change colour to grey
+    //change color to grey
 
 };
 var button = document.querySelector('#Birne');
